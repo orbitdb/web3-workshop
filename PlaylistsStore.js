c2f31b78def2573db8588eb49f38364d3ef25544
shamb0t <shams.methnani@gmail.com>
2019-08-19 10:46:25 +0200

    step 1 - set up orbitdb

---
+++ PlaylistsStore.js
@@ -0,0 +1,21 @@
+import { observable } from 'mobx'
+import Identities from 'orbit-db-identity-provider'
+import OrbitDB from 'orbit-db'
+
+class PlaylistsStore {
+  constructor () {
+    this.ipfs = null
+    this.odb = null
+    this.feed = null
+  }
+
+  async connect(ipfs, options = {}) {
+    //set up orbitdb
+    this.ipfs = ipfs
+    const identity = options.identity || await Identities.createIdentity({ id: 'user' })
+    this.odb = await OrbitDB.createInstance(ipfs, { identity, directory: './odb'})
+  }
+}
+
+const store = window.store = new PlaylistsStore()
+export default store
--- index.js
+++ index.js
@@ -2,8 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './styles/index.scss'
+import IPFS from 'ipfs'
+import store from './PlaylistsStore'
 
 class App extends React.Component{
+    async componentDidMount () {
+      const ipfs = await IPFS.create({
+        repo: './ipfs-repo',
+        EXPERIMENTAL: { pubsub: true },
+        preload: { "enabled": false },
+        config: {
+          Addresses: {
+            Swarm: ["/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star"]
+          }
+        }
+      })
+      await store.connect(ipfs)
+    }
+
     render(){
       return (
         <div>
